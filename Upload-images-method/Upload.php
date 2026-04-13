501fdf6fea5152d4d62b486ba28bf606f20ecc72
Faran <faranpannu02@gmail.com>
2026-04-13 14:03:19 +0500

    php prectice code

--- Upload-images-method/Upload.php
+++ Upload-images-method/Upload.php
@@ -28,6 +28,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
     
@@ -39,14 +43,14 @@
      <button type="submit" name="submit">Submit</button>
    </form>
 
-   <div>
+   <div class="card">
     <?php
      $res=mysqli_query($con,"select * from images");
      while($row=mysqli_fetch_assoc($res)){
 
     
     ?>
-    <img src="upimages/<?php echo $row['file']?>" alt="<?php echo $row?>">
+    <img class="rounded card-header" src="upimages/<?php echo $row['file']?>" alt="<?php echo $row?>">
   <?php }?>
 </div>
 </body>
